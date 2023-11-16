a2e3f65c2ead2f873d9630c387c2fc3117872a0a
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2023-11-16 23:38:33 +0100

    v.1

---
+++ src/lib/components/Navigatie.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+
+<section>
+<header>
+    <article>
+        <img src="/" alt="gebruiker_img">
+        <h2>User name</h2>
+        <a href="/">Bekijk account</a>
+    </article>
+    <input type="search" name="zoeken" id="zoek" placeholder="Zoek..">
+</header>
+
+<nav>
+    <ul>
+        <li>Welkom</li>
+        <li>Leeslijst</li>
+        <li>Uitleningen en verlengen</li>
+        <li>Uitleengeschiedenis </li>
+        <li>Betaalgeschiedenis</li>
+        <li>Reserveringen</li>
+        <li>Desposito's</li>
+    </ul>
+</nav>
+
+<a href="/uitloggen">Uitloggen</a>
+<a href="/"> Dark mode <span></span> </a>
+
+</section>
+
+<style>
+
+
+</style>
